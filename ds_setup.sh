4c85161bce68c78dca0ebb5d7c3594d009d460b0
esinkovets <esinkovets@dataarmor.ru>
2020-12-30 18:09:36 +0300

    Update ds_setup.sh

--- ds_setup.sh
+++ ds_setup.sh
@@ -47,15 +47,9 @@ cd /opt/datasunrise
 setupProxy() {
 
   cd /opt/datasunrise/cmdline
-  
-  if [ "$3" = "mysql" ]; then
-                            
-    XTRA_ARGS="-ssl"
-  
-  fi
 
-  ./executecommand.sh addInstancePlus -name $1 -dbPort $2 -dbType $3 -dbHost $4 -database $5 -login $6 -password $7 -proxyHost `hostname -I` -proxyPort $8 -savePassword ds $XTRA_ARGS
-
+  ./executecommand.sh addInstancePlus -name $1 -dbPort $2 -dbType $3 -dbHost $4 -database $5 -login $6 -password $7 -proxyHost `hostname -I` -proxyPort $8 -savePassword ds 
+  
 }
 
 setupDSLicense() {
