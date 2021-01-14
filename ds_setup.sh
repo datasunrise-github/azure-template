ca5397587df4e1fa3af78a6f87cdc8bfbd5a0652
esinkovets <esinkovets@dataarmor.ru>
2021-01-14 16:07:54 +0300

    Update ds_setup.sh

--- ds_setup.sh
+++ ds_setup.sh
@@ -84,7 +84,7 @@ checkInstanceExists() {
 
   instanceExists=
   
-  for attempts in {1..60}
+  for attempts in {1..70}
   do
     
     instances=`$1/cmdline/executecommand.sh showInstances`
@@ -121,7 +121,7 @@ copyProxies() {
   
   logBeginAct "Copy proxy..."
   
-  for attempts in {1..60}
+  for attempts in {1..70}
   do
     
     instances=`$1/cmdline/executecommand.sh showInstances`
